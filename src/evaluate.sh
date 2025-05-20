c20cc02e249cfedb3ac404477111b808ad9019bb
HyunjoonCho <jhj09016@gmail.com>
2025-05-20 15:52:59 +0900

    add evaluated results for voting-aware prompts

--- src/evaluate.sh
+++ src/evaluate.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-python evaluator.py -b APPS -r ../results/APPS_llama3.json
-python evaluator.py -b APPS -r ../results/APPS_llama3.1.json
-python evaluator.py -b APPS -r ../results/APPS_mistral-nemo.json
-python evaluator.py -b APPS -r ../results/APPS_qwen2.5-coder.json
+python evaluator.py -b HumanEval -r ../results/HumanEval_llama3_instruction.json
+python evaluator.py -b HumanEval -r ../results/HumanEval_llama3_rule.json
+python evaluator.py -b HumanEval -r ../results/HumanEval_llama3.1_instruction.json
+python evaluator.py -b HumanEval -r ../results/HumanEval_llama3.1_rule.json
+python evaluator.py -b HumanEval -r ../results/HumanEval_mistral-nemo_instruction.json
+python evaluator.py -b HumanEval -r ../results/HumanEval_mistral-nemo_rule.json
+python evaluator.py -b HumanEval -r ../results/HumanEval_qwen2.5-coder_instruction.json
+python evaluator.py -b HumanEval -r ../results/HumanEval_qwen2.5-coder_rule.json
+
+python evaluator.py -b APPS -r ../results/APPS_llama3_instruction.json
+python evaluator.py -b APPS -r ../results/APPS_llama3_rule.json
+python evaluator.py -b APPS -r ../results/APPS_llama3.1_instruction.json
+python evaluator.py -b APPS -r ../results/APPS_llama3.1_rule.json
+python evaluator.py -b APPS -r ../results/APPS_mistral-nemo_instruction.json
+python evaluator.py -b APPS -r ../results/APPS_mistral-nemo_rule.json
+python evaluator.py -b APPS -r ../results/APPS_qwen2.5-coder_instruction.json
+python evaluator.py -b APPS -r ../results/APPS_qwen2.5-coder_rule.json
