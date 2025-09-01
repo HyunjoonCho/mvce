011fe316a7ed433211781b4b94fb8fbfd219d29a
HyunjoonCho <jhj09016@gmail.com>
2025-09-01 14:57:03 +0900

    add phi4 code generation results

--- pass/pass.sh
+++ pass/pass.sh
@@ -1,11 +1,15 @@
-python pass.py -r ../results/HumanEval_gpt-4o_base.json 
-python pass.py -r ../results/HumanEval_llama3_base.json 
-python pass.py -r ../results/HumanEval_llama3.1_base.json 
-python pass.py -r ../results/HumanEval_mistral-nemo_base.json 
-python pass.py -r ../results/HumanEval_qwen2.5-coder_base.json 
+#!/bin/bash
 
-python pass.py -r ../results/APPS_gpt-4o_base.json 
-python pass.py -r ../results/APPS_llama3_base.json 
-python pass.py -r ../results/APPS_llama3.1_base.json 
-python pass.py -r ../results/APPS_mistral-nemo_base.json 
-python pass.py -r ../results/APPS_qwen2.5-coder_base.json 
+# python pass.py -r ../results/HumanEval_gpt-4o_base.json 
+# python pass.py -r ../results/HumanEval_llama3_base.json 
+# python pass.py -r ../results/HumanEval_llama3.1_base.json 
+# python pass.py -r ../results/HumanEval_mistral-nemo_base.json 
+# python pass.py -r ../results/HumanEval_qwen2.5-coder_base.json 
+python pass.py -r ../results/HumanEval_phi4_base.json 
+
+# python pass.py -r ../results/APPS_gpt-4o_base.json 
+# python pass.py -r ../results/APPS_llama3_base.json 
+# python pass.py -r ../results/APPS_llama3.1_base.json 
+# python pass.py -r ../results/APPS_mistral-nemo_base.json 
+# python pass.py -r ../results/APPS_qwen2.5-coder_base.json 
+python pass.py -r ../results/APPS_phi4_base.json 
--- src/evaluate.sh
+++ src/evaluate.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-python evaluator.py -b HumanEval -r ../results/HumanEval_llama3_base.json
-python evaluator.py -b HumanEval -r ../results/HumanEval_llama3.1_base.json
-python evaluator.py -b HumanEval -r ../results/HumanEval_mistral-nemo_base.json
-python evaluator.py -b HumanEval -r ../results/HumanEval_qwen2.5-coder_base.json
-python evaluator.py -b HumanEval -r ../results/HumanEval_gpt-4o_base.json
+# python evaluator.py -b HumanEval -r ../results/HumanEval_llama3_base.json
+# python evaluator.py -b HumanEval -r ../results/HumanEval_llama3.1_base.json
+# python evaluator.py -b HumanEval -r ../results/HumanEval_mistral-nemo_base.json
+# python evaluator.py -b HumanEval -r ../results/HumanEval_qwen2.5-coder_base.json
+python evaluator.py -b HumanEval -r ../results/HumanEval_phi4_base.json
+# python evaluator.py -b HumanEval -r ../results/HumanEval_gpt-4o_base.json
 
-python evaluator.py -b APPS -r ../results/APPS_llama3_base.json
-python evaluator.py -b APPS -r ../results/APPS_llama3.1_base.json
-python evaluator.py -b APPS -r ../results/APPS_mistral-nemo_base.json
-python evaluator.py -b APPS -r ../results/APPS_qwen2.5-coder_base.json
-python evaluator.py -b APPS -r ../results/APPS_gpt-4o_base.json
+# python evaluator.py -b APPS -r ../results/APPS_llama3_base.json
+# python evaluator.py -b APPS -r ../results/APPS_llama3.1_base.json
+# python evaluator.py -b APPS -r ../results/APPS_mistral-nemo_base.json
+# python evaluator.py -b APPS -r ../results/APPS_qwen2.5-coder_base.json
+python evaluator.py -b APPS -r ../results/APPS_phi4_base.json
+# python evaluator.py -b APPS -r ../results/APPS_gpt-4o_base.json
