5082ee00751b910516244c2ce06928c461822b65
HyunjoonCho <jhj09016@gmail.com>
2025-09-02 14:49:15 +0900

    preprocess phi4 HEX results

--- src/format.sh
+++ src/format.sh
@@ -1,5 +1,8 @@
-python format.py -r ../results/HumanEval-X_gpt-4o_base.json
-python format.py -r ../results/HumanEval-X_llama3_base.json
-python format.py -r ../results/HumanEval-X_llama3.1_base.json
-python format.py -r ../results/HumanEval-X_mistral-nemo_base.json
-python format.py -r ../results/HumanEval-X_qwen2.5-coder_base.json
+#!/bin/bash
+
+# python format.py -r ../results/HumanEval-X_gpt-4o_base.json
+# python format.py -r ../results/HumanEval-X_llama3_base.json
+# python format.py -r ../results/HumanEval-X_llama3.1_base.json
+# python format.py -r ../results/HumanEval-X_mistral-nemo_base.json
+# python format.py -r ../results/HumanEval-X_qwen2.5-coder_base.json
+python format.py -r ../results/HumanEval-X_phi4_base.json
