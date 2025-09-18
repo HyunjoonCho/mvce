0a922db488fb0d3f9fe114178e0a7857319ab0e3
HyunjoonCho <jhj09016@gmail.com>
2025-09-18 11:13:12 +0900

    preprocess gemini HE-X results

--- src/format.sh
+++ src/format.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-python format.py -r ../results/HumanEval-X_gpt-4o_base.json
-python format.py -r ../results/HumanEval-X_llama3_base.json
-python format.py -r ../results/HumanEval-X_llama3.1_base.json
-python format.py -r ../results/HumanEval-X_mistral-nemo_base.json
-python format.py -r ../results/HumanEval-X_qwen2.5-coder_base.json
-python format.py -r ../results/HumanEval-X_phi4_base.json
+# python format.py -r ../results/HumanEval-X_gpt-4o_base.json
+# python format.py -r ../results/HumanEval-X_llama3_base.json
+# python format.py -r ../results/HumanEval-X_llama3.1_base.json
+# python format.py -r ../results/HumanEval-X_mistral-nemo_base.json
+# python format.py -r ../results/HumanEval-X_qwen2.5-coder_base.json
+# python format.py -r ../results/HumanEval-X_phi4_base.json
+python format.py -r ../results/HumanEval-X_gemini-2.5-flash-lite_base.json
